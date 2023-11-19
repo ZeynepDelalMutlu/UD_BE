a47a0a3a17f19042e162b28a4cd7e038fca1d78d
Zeynep Delal Mutlu <zeynepdelalmutlu@gmail.com>
2023-11-19 18:31:47 +0300

    Try

--- src/main/java/tech/getarrays/employeemanager/repo/UploadedFileRepo.java
+++ src/main/java/tech/getarrays/employeemanager/repo/UploadedFileRepo.java
@@ -1,5 +1,8 @@
 package tech.getarrays.employeemanager.repo;
 
+/*
+ * Zeynep Delal Mutlu
+ */
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.getarrays.employeemanager.model.UploadedFile;
 
